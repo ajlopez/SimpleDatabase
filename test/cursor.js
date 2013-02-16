b96309ad2e55a9c4f4923b375aaa34740e53da81
ajlopez <webmaster@ajlopez.com>
2013-02-16 19:07:49 -0300

    Cursor with two rows

--- test/cursor.js
+++ test/cursor.js
@@ -9,9 +9,26 @@ exports['Cursor of one Row'] = function (test) {
     
     cursor.next(function (row) {
         total += row.age;
+        return true;
     });
     
 	test.equal(total, 800);    
+    
+	test.done();
+};
+
+exports['Cursor of Two Rows'] = function (test) {
+	var table = sdb.database('test', { create: true }).table('customers');
+    table.insert([{ name: 'Adam', age: 800 }, { name: 'Eve', age: 600 }]).run();
+    var cursor = table.run();
+    var total = 0;
+    
+    cursor.next(function (row) {
+        total += row.age;
+        return true;
+    });
+    
+	test.equal(total, 1400);    
     
 	test.done();
 };
