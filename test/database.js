446404acb7d99617962dd0f1ec6a0f89bc84527f
ajlopez <webmaster@ajlopez.com>
2013-02-16 18:05:30 -0300

    DatabaseRef with test

--- test/database.js
+++ test/database.js
@@ -6,3 +6,12 @@ exports['Create Database'] = function (test) {
 	test.ok(db);
 	test.done();
 };
+
+exports['Create and Get Database'] = function (test) {
+	var db = sdb.database('test').run();
+    var db2 = sdb.database('test').run();
+    
+    test.ok(db === db2);
+
+	test.done();
+};
