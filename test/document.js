437a1bca558d08bff67d4a6019b93be6b4c36fa7
ajlopez <webmaster@ajlopez.com>
2013-02-17 06:39:23 -0300

    copy document supports array

--- test/document.js
+++ test/document.js
@@ -32,6 +32,35 @@ exports['Insert and Get an Invariant Document'] = function (test) {
 	test.done();
 };
 
+
+exports['Insert and Get an Invariant Document with Array Values'] = function (test) {
+    var table = getTable();
+    var doc = { name: 'Adam', values: [1,2,3] };
+	var result = table.insert(doc).run();
+
+	test.ok(result);
+    test.equal(result.inserted, 1);
+    test.equal(result.errors, 0);
+    test.ok(result.keys);
+    test.equal(result.keys.length, 1);
+    test.equal(result.keys[0], 1);
+    
+    test.ok(doc.id === undefined);
+    
+    doc.name = 'New Adam';
+    doc.values.push(4);
+    
+    var newdoc = table.get(1).run();
+    
+    test.ok(newdoc);
+    test.equal(newdoc.id, 1);
+    test.equal(newdoc.name, 'Adam');
+    test.equal(newdoc.values.length, 3);
+    test.deepEqual(newdoc.values, [1,2,3]);
+
+	test.done();
+};
+
 exports['Get a Copy of a Document'] = function (test) {
     var table = getTable();
     var doc = { name: 'Adam' };
