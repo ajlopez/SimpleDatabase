c5a0b5762ee00a8defd40e839fd9e695e45fb743
ajlopez <webmaster@ajlopez.com>
2013-02-17 07:55:11 -0300

    dbList with test

--- test/database.js
+++ test/database.js
@@ -18,3 +18,19 @@ exports['Create and Get Database'] = function (test) {
 
 	test.done();
 };
+
+exports['Create and List Databases'] = function (test) {
+	sdb.dbCreate('test').run();
+	sdb.dbCreate('sales').run();
+
+    var query = sdb.dbList().run();
+    
+    query.collect(function (results) {
+        test.ok(results);
+        test.equal(results.length, 2);
+        test.ok(results.indexOf('test') >= 0);
+        test.ok(results.indexOf('sales') >= 0);
+    });
+
+	test.done();
+};
