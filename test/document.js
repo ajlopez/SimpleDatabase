01d9932fc089ad430d7ea914950293ae3c1caa48
ajlopez <webmaster@ajlopez.com>
2013-02-17 06:47:06 -0300

    Insert Document with nested document

--- test/document.js
+++ test/document.js
@@ -32,7 +32,6 @@ exports['Insert and Get an Invariant Document'] = function (test) {
 	test.done();
 };
 
-
 exports['Insert and Get an Invariant Document with Array Values'] = function (test) {
     var table = getTable();
     var doc = { name: 'Adam', values: [1,2,3] };
@@ -57,6 +56,33 @@ exports['Insert and Get an Invariant Document with Array Values'] = function (te
     test.equal(newdoc.name, 'Adam');
     test.equal(newdoc.values.length, 3);
     test.deepEqual(newdoc.values, [1,2,3]);
+
+	test.done();
+};
+
+exports['Insert and Get an Invariant Document with Nested Document'] = function (test) {
+    var table = getTable();
+    var doc = { name: 'Adam', wife: { name: 'Eve' } };
+	var result = table.insert(doc).run();
+
+	test.ok(result);
+    test.equal(result.inserted, 1);
+    test.equal(result.errors, 0);
+    test.ok(result.keys);
+    test.equal(result.keys.length, 1);
+    test.equal(result.keys[0], 1);
+    
+    test.ok(doc.id === undefined);
+    
+    doc.name = 'New Adam';
+    doc.wife.name = 'Lilith';
+    
+    var newdoc = table.get(1).run();
+    
+    test.ok(newdoc);
+    test.equal(newdoc.id, 1);
+    test.equal(newdoc.name, 'Adam');
+    test.equal(newdoc.wife.name, 'Eve');
 
 	test.done();
 };
