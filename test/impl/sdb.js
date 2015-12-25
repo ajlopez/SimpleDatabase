251e8ac35c4969f377dfccf4a85815524821c02e
ajlopez <webmaster@ajlopez.com>
2015-12-25 19:02:40 -0300

    Table wrapper in internal implementation

--- test/impl/sdb.js
+++ test/impl/sdb.js
@@ -27,6 +27,9 @@ exports['create author table'] = function (test) {
     .then(function (db, next) {
         db.createTable('authors', next);
     })
+    .then(function (table, next) {
+        table.run(next);
+    })
     .then(function (data, next) {
         test.ok(data);
         test.equal(typeof data, 'object');
@@ -35,6 +38,32 @@ exports['create author table'] = function (test) {
         test.ok(Array.isArray(data.config_changes));
         test.ok(data.tables_created);
         test.equal(data.tables_created, 1);
+        
+        test.done();
+    })
+    .run();
+};
+
+exports['create existing author table'] = function (test) {
+    test.async();
+    
+    async()
+    .then(function (data, next) {
+        sdb.db('test', next);
+    })
+    .then(function (db, next) {
+        db.createTable('authors', next);
+    })
+    .then(function (table, next) {
+        table.run(next);
+    })
+    .then(function (data, next) {
+        test.ok(data);
+        test.equal(typeof data, 'object');
+        
+        test.ok(data.config_changes);
+        test.ok(Array.isArray(data.config_changes));
+        test.equal(data.tables_created, 0);
         
         test.done();
     })
