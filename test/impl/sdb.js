a9af9ed687768f750b65204124fbd0debf34af4d
ajlopez <webmaster@ajlopez.com>
2015-12-25 19:16:22 -0300

    Table insert implementation

--- test/impl/sdb.js
+++ test/impl/sdb.js
@@ -70,3 +70,41 @@ exports['create existing author table'] = function (test) {
     .run();
 };
 
+exports['insert authors'] = function (test) {
+    test.async();
+    
+    async()
+    .then(function (data, next) {
+        sdb.db('test', next);
+    })
+    .then(function (db, next) {
+        db.table('authors', next);
+    })
+    .then(function (table, next) {
+        table.insert([
+            { name: 'Adam', age: 800 },
+            { name: 'Eve', age: 700 },
+            { name: 'Abel', age: 600 }
+        ], next);
+    })
+    .then(function (insert, next) {
+        insert.run(next);
+    })
+    .then(function (data, next) {
+        test.ok(data);
+        test.equal(typeof data, 'object');
+        
+        test.equal(data.unchanged, 0);
+        test.equal(data.skipped, 0);
+        test.equal(data.replaced, 0);
+        test.equal(data.inserted, 3);
+        test.ok(data.generated_keys);
+        test.ok(Array.isArray(data.generated_keys));
+        test.equal(data.generated_keys.length, 3);
+        test.equal(data.errors, 0);
+        test.equal(data.deleted, 0);
+        
+        test.done();
+    })
+    .run();
+};
