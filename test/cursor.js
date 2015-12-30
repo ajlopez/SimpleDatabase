f0ed1ff95fb362563ad77650481ae9d0c467e8fc
ajlopez <webmaster@ajlopez.com>
2015-12-30 08:10:09 -0300

    Fixing cursor tests, WIP

--- test/cursor.js
+++ test/cursor.js
@@ -1,11 +1,40 @@
 
 var sdb = require('../');
+var async = require('simpleasync');
 
-function getTable() {
-    sdb.dbCreate('test').run();
-    var db = sdb.db('test');
-    db.tableCreate('customers').run();
-    return db.table('customers');
+var connection;
+
+exports['get connection'] = function (test) {
+    test.async();
+    
+    sdb.connection(function (err, conn) {
+        test.ok(!err);
+        test.ok(conn);
+        
+        test.equal(typeof conn, 'object');
+        
+        connection = conn;
+        
+        test.done();
+    });
+};
+
+exports['Create table'] = function (test) {
+    test.async();
+    
+    async()
+    .exec(function (next) { sdb.db('test').tableCreate('users').run(connection, next); })
+    .exec(function (data, next) {
+        test.ok(data);
+        test.equal(typeof data, 'object');
+        
+        test.ok(data.config_changes);
+        test.ok(Array.isArray(data.config_changes));
+        test.ok(data.tables_created);
+        test.equal(data.tables_created, 1);
+        
+        test.done();
+    });
 }
 
 exports['Cursor of one Document'] = function (test) {
